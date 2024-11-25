f64026cafc14164d5fac6930d3611af1bae5d2f9
AreebAhmed14 <areebfarooq549@gmail.com>
2024-11-24 20:04:53 -0800

    menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import { Roboto } from "next/font/google";
 import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
+// import Link from "next/link";
 
 const roboto = Roboto({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    {
+      if(isMenuOpen==false){
+        setIsMenuOpen(true);
+      }
+      else{
+        setIsMenuOpen(false);
+      }
+    }
+  };
+
   return (
     <>
       <div className="h-[72px]  w-full bg-[#F7F7F7] border-b-[1px] border-b-[#676767] flex justify-between max-[656px]:justify-center items-center">
@@ -19,7 +36,16 @@ const Header = () => {
             height={1500}
             className="w-auto h-auto "
           />
-          <IoIosMenu className="hidden mt-2 mr-3 max-[656px]:block w-[35px] h-[30px]" />
+          <IoIosMenu className="hidden mt-2 mr-3 max-[656px]:block w-[35px] h-[30px]" onClick={toggleMenu}/>
+        <ul className={`${isMenuOpen ? "block" : "hidden"} w-full h-[400px] flex flex-col items-center bg-gray-200 absolute mt-[3rem] left-0`}>
+          <li className="text-[22px] mt-10 mb-4 text-white">Home</li>
+          <li className="text-[22px] my-4 text-white">Courses</li>
+          <li className="text-[22px] my-4 text-white">Service</li>
+          <li className="text-[22px] my-4 text-white">Achivement</li>
+          <li className="text-[22px] my-4 text-white">About Us</li>
+        </ul>
+
+
         </div>
         <div className="w-[910px] bg-purple-500 max-[1180px]:w-[750px]  max-[656px]:hidden h-[44px] max-[986px]:w-[600px] max-[870px]:w-[500px] max-[756px]:w-[440px] max-[756px]:mr-[2rem] mr-[3rem] flex items-center justify-end">
           <ul className="flex items-center justify-between w-full h-full bg-white">
@@ -46,6 +72,7 @@ const Header = () => {
               </li>
             </div>
           </ul>
+
         </div>
       </div>
     </>
